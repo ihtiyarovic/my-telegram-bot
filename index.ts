65c0aa59e46008b30ef88609ac88ea65136ae0b0
ihtiyarovic <ddfdfdf476@gmail.com>
2024-12-21 12:02:06 +0500

    third commit

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
-import https from "https"
 import dotenv from "dotenv"
 import fs from "fs"
 const acrcloud = require("acrcloud")
 import { Telegraf } from "telegraf"
+const ffmpeg = require('fluent-ffmpeg');
 import axios from "axios"
 dotenv.config()
 
@@ -18,92 +18,63 @@ const acr = new acrcloud({
 
 
 
-if (!BOT_TOKEN) {
-    console.log("YOUR BOT TOKEN ISN'T WORKING")
-} else {
+const options = {
+    method: 'GET',
+    url: 'https://api-name.p.rapidapi.com/endpoint',
+    headers: {
+        'X-RapidAPI-Key': 'b7b32ab4camshfdb42f836ff0e2dp1bbae3jsnd2c92dd8bb55',
+        'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+    },
+    params: {
+        key1: 'value1',
+        key2: 'value2'
+    }
+};
 
+// const download = (url, path, callback) => {
+//     request.head(url, (err, res, body) => {
+//         request(url).pipe(fs.createWriteStream(path)).on('close', callback);
+//     });
+// };
+
+
+
+if (!BOT_TOKEN) {
+    console.log("YOUR BOT TOKEN iSN'T VALID")
+} else {
     const bot = new Telegraf(BOT_TOKEN)
+
     bot.start((ctx) => {
         ctx.reply("Welcome to our bot!")
+
+        // axios.request(options).then(res => {
+        //     console.log(res)
+        // }).catch(error => {
+        //     console.error(error)
+        // })
+
         if (!chatIDs.includes(ctx.update.message.chat.id)) {
             chatIDs.push(ctx.update.message.chat.id)
         }
-
     })
 
-    bot.on("video", (ctx) => {
+    bot.on("video", async (ctx) => {
 
-        try {
-            const videoID = ctx.update.message.video.file_id
-            ctx.telegram.getFileLink(videoID).then(async (link) => {
-                https.get(link, (response) =>
-                    response.pipe(fs.createWriteStream(`./temp/${videoID}.mp4`))
-                );
+        const video = await ctx.telegram.getFile(ctx.update.message.video.file_id)
+        console.log(video)
+        // console.log(ctx.update.message.video.file_id)
 
-                const recognizeAudio = async (sample: string) => {
-                    try {
-
-                        const path = fs.readFileSync(`./temp/${videoID}.mp4`)
-
-                        const response = await acr.identify(path);
-
-                        if (response.status.code === 0) {
-                            console.log('Recognized:', response.metadata.music[0]);
-                            getShazamDetails(response.metadata.music[0].artists[0].name,
-                                response.metadata.music[0].title).then((track) => {
-                                    if (track) {
-                                        console.log("Track found:", track);
-                                    } else {
-                                        console.log("Track not found.");
-                                    }
-                                });
-                        } else {
-                            console.log('Recognition failed:', response.status.msg);
-                        }
-                    } catch (error: any) {
-                        console.error('Error:', error.message);
-                    }
-                };
-                recognizeAudio(`./songs/${videoID}`)
-            });
-        } catch (error) {
-            ctx.reply("Unable to find the song")
-            console.log(error)
-        }
+        // ffmpeg('input.mp4')
+        //     .output('output.avi')
+        //     .on('end', () => {
+        //         console.log('Conversion complete');
+        //     })
+        //     .on('error', (err: any) => {
+        //         console.error('Error: ', err.message);
+        //     })
+        //     .run();
 
     })
-
 
     bot.launch()
-}
-
-
-// Replace with your RapidAPI key
-
-async function getShazamDetails(artist: string, title: string) {
-    const query = `${title} ${artist}`;
-    const options = {
-        method: "GET",
-        url: "https://shazam.p.rapidapi.com/search",
-        params: { term: query, locale: "en-US", offset: "0", limit: "1" },
-        headers: {
-            "X-RapidAPI-Key": RAPIDAPI_KEY,
-            "X-RapidAPI-Host": "shazam.p.rapidapi.com",
-        },
-    };
-
-    try {
-        const response = await axios.request(options);
-        const track = response.data.tracks?.hits[0]?.track;
-
-        if (!track) {
-            console.log("No results found.");
-            return null;
-        }
-
-        return track;
-    } catch (error: any) {
-        console.error("Error fetching Shazam data:", error.message);
-        return null;
-    }
 }
